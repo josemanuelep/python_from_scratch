df46a3aed7aaba5bb8c3a5488efdd19e8bf9fe5c
josemanuelep <palacio.90@hotmail.com>
2020-11-25 22:44:09 -0500

    Logger example

--- Examples/main.py
+++ Examples/main.py
@@ -1,5 +1,9 @@
 import requests
+import logging
 
 response = requests.get('https://httpbin.org/ip')
 
 print('Your IP is {0}'.format(response.json()['origin']))
+
+logging.warning('Watch out!')  # will print a message to the console
+logging.info('I told you so')  # will not print anything
