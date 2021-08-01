52b3fdb9d7d69b27a6efa4c5598557327ca13ca4
josemanuelep <palacio.90@hotmail.com>
2021-08-01 18:17:21 -0500

    Ajuste, passing

--- Examples/main.py
+++ Examples/main.py
@@ -1,6 +1,17 @@
 import requests
 import logging
 
-response = requests.get('https://httpbin.org/ip')
+# Default argument value
+def for_loop_example(number=0):   
+    for i in range(number):
+        logging.warning(i)
+    if(number==1):
+        get_my_ip()
 
-logging.warning('Your IP is {0}'.format(response.json()['origin']))
+def get_my_ip():
+    response = requests.get('https://httpbin.org/ip')
+    logging.warning('Your IP is {0}'.format(response.json()['origin']))
+
+
+inputText = input('Please enter a number:')
+for_loop_example(int(inputText))
